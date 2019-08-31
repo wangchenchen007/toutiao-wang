9556ebe6ee418da258255dcd1cf4c232ad0c33e6
王晨 <939507498@qq.com>
2019-08-31 23:00:42 +0800

    自动校验手动校验完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,21 +4,21 @@
       <div class="logo">
         <img src="../../assets/img/logo_index.png" />
       </div>
-      <el-form style="margin-top :20px">
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top :20px">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
 
-        <el-form-item>
-          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:280px"></el-input>
           <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
 
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="check">
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%">登录</el-button>
+          <el-button @click="login" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -26,7 +26,63 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    let validator = function (rule, value, callBack) {
+      if (value) {
+        callBack() // 如果value为true直接通过
+      } else {
+        callBack(new Error('您必须无条件同意被坑'))
+      }
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [
+          {
+            // required只校验空字符串null  '' undefined ,不校验false和true
+            required: true,
+            message: '手机号不能为空'
+          },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式不正确'
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '验证码不能为空'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '验证码必须为6位数字'
+          }
+        ],
+        check: [
+          {
+            validator: validator
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate((isOk, obj) => {
+        if (isOk) {
+          this.$message({ type: 'success', message: '成功' })
+        } else {
+          this.$message({ type: 'warning', message: '失败' })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
