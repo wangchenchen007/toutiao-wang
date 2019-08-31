7721ca91e016ae7274572730e11ae624c1e01378
王晨 <939507498@qq.com>
2019-08-31 21:14:12 +0800

    login静态页面完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import './assets/style/index.less'
+import 'element-ui/lib/theme-chalk/index.css'
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
