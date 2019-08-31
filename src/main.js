1b2def2c07ed97c9ece38d8f9186f834df64f8ab
王晨 <939507498@qq.com>
2019-08-31 23:11:08 +0800

    axios安装 引用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import './assets/style/index.less'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http">http://ttapi.research.itcast.cn/mp/v1_0'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
