04e2abbaa7dd3506beaac246bb629b1683fad2d7
王晨晨 <939507498@qq.com>
2019-09-03 21:15:59 +0800

    评论状态 操作 formatter 作用于插槽

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -5,10 +5,19 @@
     </bread-crumb>
     <el-table :data="list">
       <el-table-column prop="title" label="标题" width="700px"></el-table-column>
-      <el-table-column prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column :formatter="text" prop="comment_status" label="评论状态"></el-table-column>
       <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
       <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-      <el-table-column label="操作"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="obj">
+          <el-button size="small" type="text">修改</el-button>
+          <el-button
+            :style="{color:obj.row.comment_status ? '#E6A23C':'#409EFF'}"
+            size="small"
+            type="text"
+          >{{obj.row.comment_status ? '关闭评论': '打开评论'}}</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </el-card>
 </template>
@@ -21,6 +30,9 @@ export default {
     }
   },
   methods: {
+    text (row) {
+      return row.comment_status ? '正常' : '关闭'
+    },
     getComments () {
       // query 路径参数
       this.$axios({
